34f8c61157472363d96ef6a158e453ad71687322
Marcelo <chelo_ge@hotmail.com>
2020-05-07 01:20:14 +0000

    models training concluded

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 import numpy as np 
+import cv2
+import random
+
 
 def get_batches(batch_size, features, labels):
     """
@@ -19,3 +22,84 @@ def get_batches(batch_size, features, labels):
         output_batches.append(batch)
         
     return np.array(output_batches)
+
+def rotate_images_randomly(images_batch):
+    new_batch = []
+    if images_batch.shape[3]==1:
+        for image in images_batch:
+            randNo = random.randint(1,5)
+            if randNo == 1:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE).reshape(32,32,1))
+            elif randNo == 2:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE).reshape(32,32,1))
+            elif randNo == 3:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_180).reshape(32,32,1))
+            else:
+                new_batch.append(image)
+    elif images_batch.shape[3]==3:
+        for image in images_batch:
+            randNo = random.randint(1,5)
+            if randNo == 1:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE).reshape(32,32,3))
+            elif randNo == 2:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE).reshape(32,32,3))
+            elif randNo == 3:
+                new_batch.append(cv2.rotate(image, cv2.ROTATE_180).reshape(32,32,3))
+            else:
+                new_batch.append(image)
+    return np.array(new_batch)
+
+#Image augmentation functions **********************************
+def transform_image(image,ang_range,shear_range,trans_range):
+
+    # Rotation
+    ang_rot = np.random.uniform(ang_range)-ang_range/2
+    rows,cols,ch = image.shape    
+    Rot_M = cv2.getRotationMatrix2D((cols/2,rows/2),ang_rot,1)
+
+    # Translation
+    tr_x = trans_range*np.random.uniform()-trans_range/2
+    tr_y = trans_range*np.random.uniform()-trans_range/2
+    Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
+
+    # Shear
+    pts1 = np.float32([[5,5],[20,5],[5,20]])
+
+    pt1 = 5+shear_range*np.random.uniform()-shear_range/2
+    pt2 = 20+shear_range*np.random.uniform()-shear_range/2
+
+    pts2 = np.float32([[pt1,5],[pt2,pt1],[5,pt2]])
+
+    shear_M = cv2.getAffineTransform(pts1,pts2)
+        
+    image = cv2.warpAffine(image,Rot_M,(cols,rows))
+    image = cv2.warpAffine(image,Trans_M,(cols,rows))
+    image = cv2.warpAffine(image,shear_M,(cols,rows))
+    
+    return image
+
+def gen_new_images(X_train,y_train,n_add,ang_range,shear_range,trans_range):
+   
+    ## checking that the inputs are the correct lengths
+    assert X_train.shape[0] == len(y_train)
+    # Number of classes: 43
+    n_class = len(np.unique(y_train))
+    X_arr = []
+    Y_arr = []
+    n_samples = np.bincount(y_train)
+
+    for i in range(n_class):
+        # Number of samples in each class
+
+        if n_samples[i] < n_add:
+            #print ("Adding %d samples for class %d" %(n_add-n_samples[i], i))
+            for i_n in range(n_add - n_samples[i]):
+                img_trf = transform_image(X_train[i_n],ang_range,shear_range,trans_range) 
+                X_arr.append(img_trf)
+                Y_arr.append(i)
+#                print ("Number of images in class %d:%f" %(i, X_arr[0])) 
+           
+    X_arr = np.array(X_arr,dtype = np.float32())
+    Y_arr = np.array(Y_arr,dtype = np.float32())
+   
+    return X_arr,Y_arr
